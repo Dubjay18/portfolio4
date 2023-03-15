ea93da428d80531bc72c45c540148b7f5f464454
Dubjay18 <jejeniyi7@gmail.com>
2023-03-15 21:43:38 +0100

    added cutom animated layout to experience page

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,24 +5,29 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import SimpleSlider from './SimpleSlider'
 import SmoothScroll from './SmoothScroll'
+import AnimatedLayout from './AnimatedLayout'
 
 function Experience() {
   return (
-    <div className="bg  flex min-h-screen items-center justify-center">
-      <div className=" mb-20">
-        <div className="my-20 flex  w-full items-center justify-center px-4">
-          {' '}
-          <hr className="mx-5 w-24 rounded-md border-t-4 border-secondary" />
-          <h1 className="prose-lg xs:text-4xl prose text-2xl font-bold text-primary md:text-6xl">
-            Experience
-          </h1>{' '}
-          <hr className="mx-5 w-24 rounded-md border-t-4 border-secondary" />
-        </div>
+    <div className="relative">
+      <AnimatedLayout>
+        <div className="bg  flex min-h-screen items-center justify-center">
+          <div className=" mb-20">
+            <div className="my-20 flex  w-full items-center justify-center px-4">
+              {' '}
+              <hr className="mx-5 w-24 rounded-md border-t-4 border-secondary" />
+              <h1 className="prose-lg xs:text-4xl prose text-2xl font-bold text-primary md:text-6xl">
+                Experience
+              </h1>{' '}
+              <hr className="mx-5 w-24 rounded-md border-t-4 border-secondary" />
+            </div>
 
-        <div className="neumophic-card mx-auto w-[80vw] py-5 ">
-          <SimpleSlider />
+            <div className="neumophic-card mx-auto w-[80vw] py-5 ">
+              <SimpleSlider />
+            </div>
+          </div>
         </div>
-      </div>
+      </AnimatedLayout>
     </div>
   )
 }
