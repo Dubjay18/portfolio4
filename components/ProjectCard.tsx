d10abb449227e145e6f10751b97f0308f8743956
Dubjay18 <jejeniyi7@gmail.com>
2023-03-20 22:59:36 +0100

    fix:modified project cards

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -10,12 +10,35 @@ interface IProps {
 function ProjectCard({ image, name, desp, liveLink, index }: IProps) {
   return (
     <div className="flex items-center">
-      <div className="z-10 max-w-[60%]">
-        <Image src={image} />
+      <div className="z-10 max-w-[60%] ">
+        <Image src={image} className="rounded-md" />
       </div>
       <div className="z-20 -ml-6">
-        <h1 className="text-right">{name}</h1>
-        <div className="rounded bg-slate-600 p-5 text-white">{desp}</div>
+        <h2 className="mb-4 text-right font-bold text-[#10182d]">{name}</h2>
+        <div className="rounded bg-[#10182d] p-5 text-white">{desp}</div>
+        <div className=" mt-5 flex items-center justify-end">
+          <a
+            href={liveLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-circle btn-ghost h-fit w-fit p-4 transition-all duration-300 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   )
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,45 +20,51 @@ function Projects() {
         </div>
         <div className="mx-auto mt-10 grid w-[80%] grid-flow-row-dense grid-cols-1 gap-5 lg:w-[99%]  xl:w-[80%] ">
           {projectData.map(({ image, name, desp, liveLink }, i) => (
+            // <motion.div
+            //   key={i}
+            //   className="group h-64 cursor-pointer rounded-xl bg-black shadow transition-all duration-500 hover:rotate-[2deg] hover:scale-95"
+            //   style={{
+            //     backgroundImage: `url(${image.src})`,
+            //     backgroundPosition: '50% 50%',
+            //     backgroundSize: 'cover',
+            //   }}
+            //   initial={{ opacity: 0, y: -40, x: 0 }}
+            //   whileInView={{ opacity: 1, y: 0, x: 0 }}
+            //   transition={{ duration: 0.5, delay: 0.2 }}
+            // >
+            //   <div className="hidden h-full items-center justify-center rounded-xl text-white transition-all duration-500 group-hover:flex group-hover:bg-black/90">
+            //     <div className="flex flex-col items-center px-6">
+            //       <span className="text-bold text-xl text-blue-600 underline decoration-dashed">
+            //         {name}
+            //       </span>
+            //       <br />
+            //       <span>{desp}</span>
+            //       <br />
+            //       <a
+            //         href={`${liveLink}`}
+            //         target="_blank"
+            //         className="btn rounded-xl bg-blue-600 text-white hover:bg-blue-700"
+            //       >
+            //         View
+            //       </a>
+            //     </div>
+            //   </div>
+            // </motion.div>
             <motion.div
               key={i}
-              className="group h-64 cursor-pointer rounded-xl bg-black shadow transition-all duration-500 hover:rotate-[2deg] hover:scale-95"
-              style={{
-                backgroundImage: `url(${image.src})`,
-                backgroundPosition: '50% 50%',
-                backgroundSize: 'cover',
-              }}
               initial={{ opacity: 0, y: -40, x: 0 }}
               whileInView={{ opacity: 1, y: 0, x: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
+              className="my-5"
             >
-              <div className="hidden h-full items-center justify-center rounded-xl text-white transition-all duration-500 group-hover:flex group-hover:bg-black/90">
-                <div className="flex flex-col items-center px-6">
-                  <span className="text-bold text-xl text-blue-600 underline decoration-dashed">
-                    {name}
-                  </span>
-                  <br />
-                  <span>{desp}</span>
-                  <br />
-                  <a
-                    href={`${liveLink}`}
-                    target="_blank"
-                    className="btn rounded-xl bg-blue-600 text-white hover:bg-blue-700"
-                  >
-                    View
-                  </a>
-                </div>
-              </div>
+              <ProjectCard
+                image={image}
+                name={name}
+                desp={desp}
+                liveLink={liveLink}
+                index={i}
+              />
             </motion.div>
-            // <div key={i}>
-            //   <ProjectCard
-            //     image={image}
-            //     name={name}
-            //     desp={desp}
-            //     liveLink={liveLink}
-            //     index={i}
-            //   />
-            // </div>
           ))}
         </div>
       </div>
